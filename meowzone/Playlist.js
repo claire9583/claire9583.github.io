33d5ef4ec820f068b0fcbb10f123b8e6dd82609e
claire <69117910+claire9583@users.noreply.github.com>
2025-07-18 18:25:11 -0400

    Update Playlist.js

--- meowzone/Playlist.js
+++ meowzone/Playlist.js
@@ -4,6 +4,7 @@ const playlist = [
     "/assets/audio/meowsongs/Just the mew of us.mp3",
     "/assets/audio/meowsongs/Earth Wind & Meower - September.mp3",
     "/assets/audio/meowsongs/Cats on Meows.mp3",
+    "/assets/audio/meowsongs/Meow - lvusm.mp3",
     "/assets/audio/meowsongs/Meowh Klahoma - Jack Stauber.mp3",
     "/assets/audio/meowsongs/Buttercup - Jack Meowber.mp3"
 ];
