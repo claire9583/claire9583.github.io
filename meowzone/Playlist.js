575b8b2ecfe46cbd8a90f3f0c6c30f3ce3357415
claire <69117910+claire9583@users.noreply.github.com>
2025-07-18 19:07:36 -0400

    Update Playlist.js

--- meowzone/Playlist.js
+++ meowzone/Playlist.js
@@ -5,7 +5,24 @@ const playlist = [
     "/assets/audio/meowsongs/Earth Wind & Meower - September.mp3",
     "/assets/audio/meowsongs/Cats on Meows.mp3",
     "/assets/audio/meowsongs/Meow - lvusm.mp3",
-    "/assets/audio/meowsongs/Meowh Klahoma - Jack Stauber.mp3"
+    "/assets/audio/meowsongs/Meowh Klahoma - Jack Stauber.mp3",
+    "/assets/audio/meowsongs/Meowin'.mp3",
+    "/assets/audio/meowsongs/Meowl City - Fireflies.mp3",
+    "/assets/audio/meowsongs/I Don't Want to Set the Meow on Fire.mp3",
+    "/assets/audio/meowsongs/Meowster Sandman.mp3",
+    "/assets/audio/meowsongs/Submeower Lullaby.mp3",
+    "/assets/audio/meowsongs/Meow Star.mp3",
+    "/assets/audio/meowsongs/Cat from Ipanema.mp3",
+    "/assets/audio/meowsongs/Fallen Meown Reprise.mp3",
+    "/assets/audio/meowsongs/Temmeow Village.mp3",
+    "/assets/audio/meowsongs/Meower Garden - Yoshi Island.mp3",
+    "/assets/audio/meowsongs/Meowster Blue Sky.mp3",
+    "/assets/audio/meowsongs/DuckTales - The Moon Theme.mp3",
+    "/assets/audio/meowsongs/Meower Meower Docks.mp3",
+    "/assets/audio/meowsongs/Coconut Meowll.mp3",
+    "/assets/audio/meowsongs/Lost Woods - The Legend of Meowda Ocarina of Time.mp3",
+    "/assets/audio/meowsongs/TF2 Meown Theme.mp3",
+    "/assets/audio/meowsongs/Gotye - Somekitty That I Used To Meow.mp3",
 ];
 
 let currentTrack = 0;
